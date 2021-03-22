2576c9c8ede507489dea1fc2b7594ad3bf188bfc
Jpop Wilson <asimovsghost@gmail.com>
2021-03-21 19:36:02 -0700

    wrap up

--- whodat/main.js
+++ whodat/main.js
@@ -1,5 +1,6 @@
 var CURRENT_POKEMON;
-
+var SCORE = 0;
+var GAME_TIME = 60;
 
 function getRandomInt(max) {
     return Math.floor(Math.random() * Math.floor(max)+1);
@@ -8,7 +9,9 @@ function getRandomInt(max) {
 function getPokemon(specific_pokemon){
 
     var max_id = 151; // max pokedex ID of pokemon
+    
     var random_pokemon = getRandomInt(max_id);
+
     if (specific_pokemon != null){
         random_pokemon = specific_pokemon;
     }
@@ -29,7 +32,10 @@ function getPokemon(specific_pokemon){
 
 function setPokemon(pokemon){
 
-    pokemon.name = pokemon.name.replace("-m", "").replace("-f", "");
+    pokemon.name = 
+    pokemon.name.replace("nidoran-m", "nidoran")
+                .replace("nidoran-f", "nidoran")
+                .replace("mr-mime", "mr mime");
 
     CURRENT_POKEMON = pokemon;
 
@@ -38,6 +44,7 @@ function setPokemon(pokemon){
 
     pokemon_name.innerHTML = pokemon.name;
     pokemon_image.src = pokemon["sprites"]["other"]["official-artwork"]["front_default"];
+    pokemon_image.classList.remove("revealed");
 
     var guess_text = document.getElementById("guess");
     guess_text.value = "";
@@ -58,6 +65,21 @@ function startGame(){
     game_controls.className = "active";
 
     // start timer
+    var countdown = setInterval(
+        function(){
+
+            GAME_TIME--;
+            var timer = document.getElementById("timer");
+            timer.innerHTML = GAME_TIME;
+
+            if(GAME_TIME <= 0){
+                clearInterval(countdown);
+                gameOver();
+            }
+
+        },
+        1000
+    );
 
 }
 
@@ -74,27 +96,101 @@ function checkGuess(guess){
 }
 
 
+
+function revealPokemon(){
+
+    var pokemon_img = document.getElementById("pokemon-image");
+    pokemon_img.classList.add("revealed");
+
+}
+
+
+
+
+function gameOver(){
+    
+    showMessage("Your final score was " + SCORE);
+
+    var guess_form = document.getElementById("guess-form");
+    guess_form.remove();
+
+    var restart_button = document.getElementById("restart");
+    restart_button.style.display = "inline-block";
+
+    var game_over_msg = document.getElementById("game-over");
+    game_over_msg.style.display = "block";
+
+
+}
+
+
+function showMessage(msg){
+    var message = document.getElementById("message");
+    message.innerHTML = msg;
+}
+
 document.addEventListener("DOMContentLoaded", function(){
 
-    var start_button = document.getElementById("start-game");
-    var guess_button = document.getElementById("submit-guess");
-    var guess_text = document.getElementById("guess");
-    
-    var guess_form = document.getElementById("guess-form");
+    var start_button    = document.getElementById("start-game");
+    var guess_button    = document.getElementById("submit-guess");
+    var guess_text      = document.getElementById("guess");
+    var guess_form      = document.getElementById("guess-form");
+    var score           = document.getElementById("score");
+    var game            = document.getElementById("game");
+    var restart_button  = document.getElementById("restart");
 
     guess_form.addEventListener("submit", function(event){
         
         event.preventDefault();
 
         if(checkGuess(guess_text.value)){
-            getPokemon();
+            // if they get it right
+            showMessage("Correct!")
+            SCORE += 100;
+            score.innerHTML = SCORE;
+            revealPokemon();
+            setTimeout(
+                function(){ 
+                    showMessage("")
+                    getPokemon(); 
+                },
+                750
+            )
+            
+
+        } else {
+            // if they get it wrong
+            game.classList.add("shake");
+            showMessage("WRONG!!!")
+            
+            guess_form.reset();
+            guess_text.focus();
+
+            setTimeout(
+                function(){ 
+                    game.classList.remove("shake"); 
+                    showMessage("");
+                },
+                500
+            );
+
+
         }
 
     });
 
+
+
+
     start_button.addEventListener("click", function(){
         
         startGame();
+
+    });
+
+    restart_button.addEventListener("click", function(){
+        
+        location.reload();
 
     });
 
